144a436323612d635d299dfb83776695eb34ab55
dim <jimstef@outlook.com>
2020-12-28 16:39:47 +0200

    heroku stuff

--- therapy/settings.py
+++ therapy/settings.py
@@ -28,7 +28,7 @@ env = environ.Env(
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
