63ad4d78ca4c0359b0d04d68e6372759824c5689
dim <jimstef@outlook.com>
2020-12-28 16:38:04 +0200

    heroku stuff

--- therapy/settings.py
+++ therapy/settings.py
@@ -22,13 +22,13 @@ env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-environ.Env.read_env()
+#environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
