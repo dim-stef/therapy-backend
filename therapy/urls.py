68c2c49375d9cb79a102d82c5bf021610e070b6d
dim <jimstef@outlook.com>
2021-01-20 03:12:03 +0200

    possible do fix

--- therapy/urls.py
+++ therapy/urls.py
@@ -13,15 +13,32 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+'''
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('', include('api.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)'''
+if os.environ.get('DEVELOPMENT_MODE') == 'True':
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+        path('rest-auth/', include('rest_auth.urls')),
+        path('rest-auth/registration/', include('rest_auth.registration.urls')),
+        path('', include('api.urls'))
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns = [
+        path('', admin.site.urls),
+        path('', include('rest_framework.urls', namespace='rest_framework')),
+        path('', include('rest_auth.urls')),
+        path('', include('rest_auth.registration.urls')),
+        path('', include('api.urls'))
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
