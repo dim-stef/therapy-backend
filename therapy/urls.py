67421d0f34b479eeadfb8058b86a9c7331249d0f
dim <jimstef@outlook.com>
2021-01-25 01:39:56 +0200

    rolled back urls

--- therapy/urls.py
+++ therapy/urls.py
@@ -18,15 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-'''
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('', include('api.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)'''
-if os.environ.get('DEVELOPMENT_MODE') == 'True':
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+'''if os.environ.get('DEVELOPMENT_MODE') == 'True':
     urlpatterns = [
         path('admin/', admin.site.urls),
         path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
@@ -41,4 +41,4 @@ else:
         path('', include('rest_auth.urls')),
         path('', include('rest_auth.registration.urls')),
         path('', include('api.urls'))
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)'''
